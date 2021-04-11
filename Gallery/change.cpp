4fd0d2a6b5ea1b05e7cf15dac50a62eb09b40cbd
ENOFOGHA <enofogha.gabriel@gmail.com>
2021-04-12 02:52:46 +0300

    first commit

---
+++ Gallery/change.cpp
@@ -0,0 +1,15 @@
+<!DOCKTYPE html>
+<html>
+<head>
+<title> website layout with image gallery</title>
+<body>
+<nav>
+    <ul>
+	   <li><a href ="#"> Home</a></li>
+	   <li><a href ="#"> News</a></li>
+	   <li><a href ="#"> Download</a></li>
+	   <li><a href ="#"> Gallery</a></li>
+	   <li><a href ="#"> Contact</a></li>
+	   
+</body>
+</html>
